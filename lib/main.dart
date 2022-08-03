b3d8111d57c83e22fa407c6ea1d44b1cc303f176
aryanmaheshwari1420 <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-08-04 02:31:49 +0530

    newfileadded

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:postapi/screens/homescreen.dart';
 import 'package:postapi/screens/photoscreen.dart';
 import 'package:postapi/screens/userscreen.dart';
+import 'package:postapi/screens/withoutModelScreenuser.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: UserScreen(),
+    home: WMuserScreen(),
   ));
 }
